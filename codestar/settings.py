3a1962c69dd17b091f3606ecb46949b01f434e9f
paulabes <paul.cem@gmail.com>
2025-01-19 17:53:53 +0000

    remove static

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v^vbqn#1ax7$yzk#kbs&paoy#d(f&python7_j+z$6a7q4da%4#9%9l8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost:8000', '127.0.0.1:8000']
+ALLOWED_HOSTS = ["8000-ajgreaves-blog-lesson-pl-lfv6kgfdws.us2.codeanyapp.com"]
 
 
 # Application definition
@@ -122,6 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Gemini fix
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static' 
+
+
