e879f6e7b4ce4894d45fcea7c3adc6876f01d25b
paulabes <paul.cem@gmail.com>
2025-01-19 18:31:04 +0000

    argh

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost:8000', 'localhost', '127.0.0.1:8000', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from blog import views as blog_views
 
 urlpatterns = [
+    
     path("blog/", blog_views.my_blog, name='blog'),
     path('admin/', admin.site.urls),
+    
 ]
