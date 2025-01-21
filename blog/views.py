42e1182fe8c2efedd3128b68b954ceb45d38542b
paulabes <paul.cem@gmail.com>
2025-01-21 12:10:24 +0000

    problems fixed

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
-
 def my_blog(request):
     return HttpResponse("Hello, Blog!")
+
+def home(request):
+    return HttpResponse("Welcome to the Home Page!")
--- procfile
+++ procfile
@@ -1,2 +1,1 @@
 web: gunicorn codestar.wsgi
-
